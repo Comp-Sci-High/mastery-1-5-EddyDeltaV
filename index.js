f10deab369a786c99fa2507cd687d7f61983d28c
EddyDeltaV <Eduardo.santos25@compscihigh.org>
2024-11-18 02:27:59 +0000

    tasks 1-5 complete

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let supplyList = ["Pencils", "Erasers"];
 
 // 1. Modularize the following code examples into a new function called addItems
 // This function should take in a new item as an input, add it to the supplyList array, console log the added item, and then return the new item
-
+function addItems(newItem){
+supplyList.push(newItem)
+}
 // Code to Modularize
 // let newItem1 = "Markers"
 // supplyList.push(newItem1)
@@ -15,18 +17,20 @@ let supplyList = ["Pencils", "Erasers"];
 
 
 // 2. Call your addItems function with "Markers" as an input. 
-
+addItems("Markers")
 
 // 3. Call your addItems function with "Tape" as an input
-
+addItems("Tape")
 // 4. Call your addItems function with "Glue" as an input
-
-
+addItems("Glue")
+console.log(supplyList)
 // 5. Write a function called remainingItems to log how many items are in the list.
 // Log: "The supply list has x item(s)." x being the amount of items. 
 // The function should return supplyList's length
-
+function remainingItems(){
+console.log("The supply list has " + supplyList.length + " item(s).")
+}
 
 // 6. Call your remainingItems function.
-
+remainingItems()
 
